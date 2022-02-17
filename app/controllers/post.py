dd0562f0d6ed3cbddd8f856e3e19d090194977e1
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-02-17 20:13:16 +0100

    remove post model, repo and controller

--- app/controllers/post.py
+++
@@ -1,9 +0,0 @@
-from app.data.repository import post as post_repo
-
-
-def create(title: str, content: str) -> post_repo.PostDB:
-    return post_repo.create(title, content)
-
-
-def get_by_title(title: str) -> post_repo.PostDB | None:
-    return post_repo.get_by_title(title)
